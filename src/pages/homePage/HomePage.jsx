6b65a1f698d3de13668422d3f34e6f16364605b6
Roberto <roberto.vukomanovic@gmail.com>
2024-08-27 18:59:51 +0200

    added visit counter

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import ImageEditor from '../../components/imageEditor/ImageEditor';
 import './homePage.scss';
+import supabase from '../../config/supabaseClient';
 
 export default function HomePage() {
+    // TODO
+    useEffect(() => {
+        const registerVisit = async () => {
+            const {error} = await supabase
+                .from('visitCount')
+                .insert([{page: "homepage"}])
+
+                if (error) {
+                    console.log(error)
+                }
+        }
+        registerVisit()
+    }, [])
 
 
     return (
--- src/pages/newPage/ParserPage.jsx
+++ src/pages/newPage/ParserPage.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
 import ChatlogParser from '../../components/chatlogParser/ChatlogParser';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import './parserPage.scss';
+import supabase from '../../config/supabaseClient';
 
 export default function ParserPage() {
+
+    // TODO
+    useEffect(() => {
+        const registerVisit = async () => {
+            const {error} = await supabase
+                .from('visitCount')
+                .insert([{page: "parser"}])
+
+                if (error) {
+                    console.log(error)
+                }
+                console.log("test")
+        }
+        registerVisit()
+    }, [])
+
     return (
         <>
         <Header />
